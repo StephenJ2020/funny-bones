548b47af7d189cfbceb4bbba57cb4699f3f143c6
Stephen <sjohnston.ie@gmail.com>
2021-10-16 03:02:27 +0000

    feat: Add functions randomTime randomHole and peep

--- assets/js/ghost.js
+++ assets/js/ghost.js
@@ -1,9 +1,29 @@
+/* jshint esversion: 6 */
+
 /* Adapted from Tutorial - https://courses.wesbos.com/account/access/6148bf70b36fe451adb8f730/view/194158577 */
 
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');  
+let lastHole;
 
-function randomtime(min, max){
-    return Math.random() * (min - max) + min;
+function randomTime(min, max){
+    return Math.round(Math.random() * (min - max) + min);
+}
+
+function randomHole(holes){
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole){
+        return randomHole(holes);
+    }
+
+    lastHole = hole;
+    return hole;
+}
+
+function peep(){
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    console.log('time, hole');
 }
