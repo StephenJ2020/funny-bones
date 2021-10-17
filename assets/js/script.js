98429fdad3ee41ac75adca1d6254320c98913736
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-10-17 12:11:32 +0000

    Add javascript to script.js in order to create basic jumping game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,82 @@
-window.onload = function(){
+window.onload = function() {
+    var character = document.getElementById("character");
+    console.log(character)
+    var block = document.getElementById("block");
+    console.log(block)
+    }
+
+
+window.onload = function() {
     console.log("boomshki")
     var lets_play = document.getElementById("play-button");
     lets_play.addEventListener("click", mad_scientist)
 }
 
 
-function mad_scientist(){
+function mad_scientist() {
     var games_console = document.getElementById("console");
     games_console.hidden = true;
     var game_grid = document.getElementById("game-grid");
-    game_grid.hidden = false;
-    make_grid()    
+    game_grid.hidden = true;
 }
 
 
-function make_grid () {
-    const grid = document.querySelector("#game-grid");
-    console.log(grid);
+function jump () {
+    if (character.classList != "animate"){
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate")
+    },500)
+}
+}
 
-    for (let i = 0; i < 225; i++){
-        const square = document.createElement('div');
-        grid.appendChild(square)
+var check_dead = setInterval(function(){
+    var game_grid = document.getElementById("game-grid");
+    var height = parseInt(window.getComputedStyle(game_grid).getPropertyValue("height"));
+    console.log(height)
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
+    console.log(characterTop);
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
+    if (blockLeft<20 && blockLeft>0 % characterTop>= height-500){
+        alert("u lose");
+        block.style.animation = "none";
+        block.style.display = "none"
     }
-    make_aliens
-}
+},10);
+    
 
 
-function make_aliens() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function get_grid() {
+    const grid = document.querySelector("#game-grid");
+    for (let i = 0; i < 225; i++){
+    const square = document.createElement('div');
+    grid.appendChild(square)
+}}
+
+
+/*function draw () {
     const squares = Array.from(document.querySelectorAll('#game-grid div'))
-    const alienInvaders = [
-        0,1,2,3,4,5,6,7,8,9,
-        15,16,17,18,19,20,21,22,23,24,
-        30,31,32,33,34,35,36,37,38,39
-    ]
-}
-
-
-function draw(){
+    console.log(squares);
     for (let i = 0; i < alienInvaders.length; i++) {
         squares[alienInvaders[i]].classList.add('invader')
     }
 }
+
+draw()
+
+squares[currentShooterIndex].classList.add('shooter')*/
+
 
